5e8db472f9fd0133c47e467b270871544c3cbe05
abhinav3254 <abhinav3254@gmail.com>
2023-10-06 23:44:48 +0530

    netflix-done

--- bank-registry/src/main/java/com/axis/BankRegistryApplication.java
+++ bank-registry/src/main/java/com/axis/BankRegistryApplication.java
@@ -2,14 +2,14 @@ package com.axis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class UserApplication {
+@EnableEurekaServer
+public class BankRegistryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserApplication.class, args);
+		SpringApplication.run(BankRegistryApplication.class, args);
 	}
 
 }
